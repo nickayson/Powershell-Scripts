135d7e1a9455f2459b30ed7f856dc32d89bdff3a
Nicholas Ayson <aysonnicholas@gmail.com>
2022-01-27 15:05:40 -0800

    Add files via upload

---
+++ GetDiskInventoryV3.ps1
@@ -0,0 +1,10 @@
+﻿param (
+    $computername = 'localhost'
+    )
+Get-WmiObject -Class Win32_LogicalDisk -ComputerName $computername `
+    -Filter "drivetype=3" |
+    Sort-Object -property DeviceID |
+    Format-Table -Property DeviceID, 
+        @{label='FreeSpace(MB)';expression={$_.FreeSpace / 1MB -as [int]}},
+        @{label='Size(GB';expression={$_.Size / 1GB -as [int]}},
+        @{label='%Free';expression={$_.FreeSpace / $_.Size * 100 -as [int]}}
