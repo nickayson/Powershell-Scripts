486d075708cbab1fc02943df28e9b226ff462937
Nicholas Ayson <aysonnicholas@gmail.com>
2022-01-27 15:26:59 -0800

    Add files via upload

--- GetDiskInventoryV3.ps1
+++ GetDiskInventoryV3.ps1
@@ -1,4 +1,25 @@
-﻿param (
+﻿<#
+.SYNOPSIS
+Get-DiskInventory retrieves logical disk information from one or
+more computers.
+.DESCRIPTION
+Get-DiskInventory uses WMI to retrieve the Win32_LogicalDisk
+instances from one or more computers. It displays each disk's
+drive letter, free space, total size, and percentage of free
+space.
+.PARAMETER computername
+The computer name, or names, to query. Default: Localhost.
+.PARAMETER drivetype
+The drive type to query. See Win32_LogicalDisk documentation
+for values. 3 is a fixed disk, and is the default.
+Listing 21.4 Adding help to Get-DiskInventory.ps1
+Using parameter
+Licensed to oz sa <ozs@mailscheap.us>
+Documenting your script 269
+.EXAMPLE
+Get-DiskInventory -computername SERVER-R2 -drivetype 3
+#>
+param (
     $computername = 'localhost',
     $drivetype = 3
     )
