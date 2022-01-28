5007218972a512711aa3f0957c99136776ea1d13
Nicholas Ayson <aysonnicholas@gmail.com>
2022-01-28 08:52:33 -0800

    Add files via upload

---
+++ GetPhysicalAdapters.ps1
@@ -0,0 +1,12 @@
+﻿[cmdletbinding()]
+Param (
+    [Parameter(Mandatory=$True, HelpMessage="Enter a computername to query")]
+    [alias('hostname')]
+    [string]$Computername
+    )
+    Write-Verbose "Getting physical network adapters from $computername" -Verbose
+    Get-WmiObject -Class win32_networkadapter -computername $Computername |
+     where { $_.PhysicalAdapter } |
+     select MACAddress,AdapterType,DeviceID,Name,Speed
+
+     Write-Verbose "Script Finised." -Verbose
